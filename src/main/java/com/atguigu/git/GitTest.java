e5f33ed2deeb0a602480487ae665278bbf2d8725
liberty <3114290781@qq.com>
2023-03-18 23:54:00 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author ZhaiLibo
+ * @date 2023/3/18 -23:49
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
